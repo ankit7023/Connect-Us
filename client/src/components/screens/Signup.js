d6a4b87d6628e880ca655c42250815019321e014
ankit7023 <68903835+ankit7023@users.noreply.github.com>
2022-10-15 14:38:56 +0530

    Update Signup.js

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -71,7 +71,7 @@ const Signup  = ()=>{
    return (
       <div className="mycard">
           <div className="card auth-card input-field">
-            <h2>Instagram</h2>
+            <h2>Connect Us</h2>
             <input
             type="text"
             placeholder="name"
