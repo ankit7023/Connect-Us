5da557e486790050b27bf72149b25e5ea2fc35ed
ankit7023 <68903835+ankit7023@users.noreply.github.com>
2022-10-15 14:37:07 +0530

    Update Newpassword.js

--- client/src/components/screens/Newpassword.js
+++ client/src/components/screens/Newpassword.js
@@ -34,7 +34,7 @@ const SignIn  = ()=>{
    return (
       <div className="mycard">
           <div className="card auth-card input-field">
-            <h2>Instagram</h2>
+            <h2>Connect Us</h2>
         
             <input
             type="password"
